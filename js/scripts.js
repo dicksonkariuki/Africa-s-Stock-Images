a9daa476cea4cac8a1f27e4f3234439e39733538
Dickson Karis <dicksonkariuki4@gmail.com>
2019-09-05 16:13:53 +0300

    Add function validate

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function validateform() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+  
+    if (email.value === "" || email.value === null) {
+      alert("Email is required");
+      return false;
+    } else if (password.value === "" || password.value === null) {
+      alert("password is required");
+      return false;
+    } else {
+      alert("Proceed to download an image");
+    }
