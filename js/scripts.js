cc183c2e04d21df043d0b9b4b6509660c18899ef
Dickson Karis <dicksonkariuki4@gmail.com>
2019-09-05 16:16:07 +0300

    Add message code

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 function validateform() {
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-  
-    if (email.value === "" || email.value === null) {
-      alert("Email is required");
-      return false;
-    } else if (password.value === "" || password.value === null) {
-      alert("password is required");
-      return false;
-    } else {
-      alert("Proceed to download an image");
-    }
-    var myInput = document.getElementById("password");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  if (email.value === "" || email.value === null) {
+    alert("Email is required");
+    return false;
+  } else if (password.value === "" || password.value === null) {
+    alert("password is required");
+    return false;
+  } else {
+    alert("Proceed to download an image");
+  }
+  var myInput = document.getElementById("password");
   var letter = document.getElementById("letter");
   var capital = document.getElementById("capital");
   var number = document.getElementById("number");
   var length = document.getElementById("length");
+  myInput.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+  };
+  myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+  };
+}
